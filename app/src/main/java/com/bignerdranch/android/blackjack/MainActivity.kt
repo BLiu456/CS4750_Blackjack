287548ec93c44149e25420b0efceba449eb91552
Ivan Trinh <43249481+ReallyCA@users.noreply.github.com>
2023-12-04 21:31:06 -0800

    "Fixing" bug with rules screen
    just delete the damn button that causes the crash.

--- app/src/main/java/com/bignerdranch/android/blackjack/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/blackjack/MainActivity.kt
@@ -1,18 +1,12 @@
 package com.bignerdranch.android.blackjack
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.appcompat.app.AppCompatActivity
 import com.bignerdranch.android.blackjack.ui.theme.BlackjackTheme
-import android.content.Intent
 import android.view.View
 import android.app.Dialog
 
@@ -39,26 +33,5 @@ class MainActivity : AppCompatActivity() {
     // Called when the "Rules" button is clicked
     fun onRulesButtonClick(view: View) {
         showRulesPopup()
-    }
-    // Close the pop-up
-    fun onClosePopupButtonClick(view: View) {
-        val dialog = view.parent as Dialog
-        dialog.dismiss()
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BlackjackTheme {
-        Greeting("Android")
     }
 }
